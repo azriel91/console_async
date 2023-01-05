6996712182369468e430d7bacfaac51f185dcbaf
Azriel Hoh <azriel91@gmail.com>
2023-01-05 14:09:03 +1300

    Convert `term` example to use tokio.

--- examples/term.rs
+++ examples/term.rs
@@ -1,35 +1,37 @@
-use std::{
-    io::{self, Write},
-    thread,
-    time::Duration,
-};
+use std::{thread, time::Duration};
 
 use console::{style, Term};
 
-fn do_stuff() -> io::Result<()> {
+async fn do_stuff() -> std::io::Result<()> {
     let term = Term::stdout();
     term.set_title("Counting...");
-    term.write_line("Going to do some counting now")?;
-    term.hide_cursor()?;
+    term.write_line("Going to do some counting now").await?;
+    term.hide_cursor().await?;
     for x in 0..10 {
         if x != 0 {
-            term.move_cursor_up(1)?;
+            term.move_cursor_up(1).await?;
         }
-        term.write_line(&format!("Counting {}/10", style(x + 1).cyan()))?;
-        thread::sleep(Duration::from_millis(400));
+        term.write_line(&format!("Counting {}/10", style(x + 1).cyan()))
+            .await?;
+        thread::sleep(Duration::from_millis(10));
     }
-    term.show_cursor()?;
-    term.clear_last_lines(1)?;
-    term.write_line("Done counting!")?;
-    writeln!(&term, "Hello World!")?;
+    term.show_cursor().await?;
+    term.clear_last_lines(1).await?;
+    term.write_line("Done counting!").await?;
+    term.write_line("Hello World!").await?;
 
-    write!(&term, "To edit: ")?;
-    let res = term.read_line_initial_text("default")?;
-    writeln!(&term, "\n{}", res)?;
+    term.write_str("To edit: ").await?;
+    let res = term.read_line_initial_text("default").await?;
+    term.write_line(format!("\n{res}").as_str()).await?;
 
     Ok(())
 }
 
 fn main() {
-    do_stuff().unwrap();
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .thread_name("main")
+        .build()
+        .unwrap();
+
+    runtime.block_on(do_stuff()).unwrap();
 }
